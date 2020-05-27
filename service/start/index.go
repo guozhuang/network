0c81941f4599845eff2fb5616615d770174a6055
guozhuang <guo3525732@163.com>
2020-05-27 20:46:19 +0800

    分层

--- service/start/index.go
+++ service/start/index.go
@@ -4,8 +4,6 @@ import (
 	"network/repository"
 )
 
-var repo repository.Repo
-
 //定义自己的interface，将整个包的方法规范
 type IStartService interface {
 	say(string) string
@@ -13,16 +11,14 @@ type IStartService interface {
 }
 
 type Service struct {
-	//引入外部的反射接口
-	Repo repository.IRepo `inject:""`
+	Repo repository.Repo
+	//Repo repository.IRepo `inject:""`
 }
 
 func (s *Service) Say(message string) string {
-	//可以使用链式调用的方式进行处理
-	return repo.GetRepoModel().Start.Speak(message)
+	return s.Repo.GetRepoModel().Start.Speak(message)
 }
 
 func (s *Service) Speak(message string) string {
-	//可以使用链式调用的方式进行处理
-	return repo.GetRepoModel().Start.Speak(message)
+	return s.Repo.GetRepoModel().Start.Speak(message)
 }
